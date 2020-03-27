2d0d1e36834ad4fc92b0d1e0798fb60e78505a03
Rudson Varona <rjvarona16@gmail.com>
2020-03-27 14:21:06 -0400

    added parser ish

--- true.js
+++ true.js
@@ -37,3 +37,134 @@ function truInterp(TrueExpr){
 var x = truInterp(truNot);
 
 console.log(x);
+
+
+
+//lexer
+// turn expr
+
+// and t or t not f
+
+//into
+
+//['and', 't', or, t, not, f]
+
+
+const lex = str => str.split(' ').map(s => s.trim()).filter(s => s.length);
+/*
+parser is responsible for turning the token list into AST
+
+['and', 't', or, t, not, f]
+
+    and
+    /   \
+    t    or
+        /  \
+        t   not
+            /
+           f 
+
+
+```
+EBNF FORM
+bool := t || f
+op := and || or || not
+expr := bool || op || expr+
+
+*/
+
+
+/*
+    this is a parser
+*/
+
+const Op = Symbol('op');
+const Bool = Symbol('bool');
+
+
+const parse = tokens => {
+
+    let c = 0;
+    const peek = () => tokens[c];
+    const consume = () => tokens[c++];
+  
+    const parseBool = () => ({ val: consume() , type: Bool });
+    
+  
+
+    const parseOp = () => {
+
+
+      const node = { 
+          val: checkRegEx(consume()), type: Op, expr: [] };
+
+
+
+      while (peek()) node.expr.push(parseExpr());
+      return node;
+    
+    
+    
+    };
+  
+    const parseExpr = () => /\d/.test(peek()) ? parseBool() : parseOp();
+    
+    return parseExpr();
+  };
+
+var not = /not/i
+var and = /and/i
+var or =  /or/i
+var regexTrue = /true/i
+var regexFalse = /false/i
+
+const truParse = tokens => {
+    let c = 0;
+
+   
+    
+    const parseBool = () => ({ val: new TrueExpr(consume()) , type: Bool });
+    
+    
+    
+    switch(regex){
+           
+        case or:
+            return  new TrueExpr(null, null, {lhs: truParse, rhs: truParse  });
+        case and:
+        
+            return  new TrueExpr(null, {lhs: truParse, rhs: truParse });
+        case not:
+            return  new TrueExpr(null, null, null, truParse);
+        case regexTrue:
+            return  new TrueExpr(true);
+                
+        case regexFalse:
+            return  new TrueExpr(false);
+    }
+    
+    
+  
+
+    const parseExpr = () => ((regexTrue.test(peek())) || (regexFalse.test(peek()))) ? parseBool() : parseOp();
+    
+    const parseOp = () => {
+        
+        console.log(consume())
+        const node = { 
+            val: checkRegEx(consume()), type: Op, expr: [] };
+        while (peek()) node.expr.push(parseExpr());
+        return node;
+    };
+
+    return parseExpr();
+}
+
+
+/*
+  developing the transpile
+*/
+
+
+  const test1 = "and or true false or true not false"
+  console.log( JSON.stringify((truParse(lex(test1)))))
